e01e3d9b2e24f14549aaab23795b13883024787e
axton21 <axton_n@yahoo.com>
2015-02-28 16:21:58 -0500

    sql

---
+++ lib/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO cats (name, age, gender, breed, temperment, declawed) VALUES ('Maru', 6, 'M','Scottish Fold', 'playful', 0);
+INSERT INTO cats (name, age, gender, breed, temperment, declawed) VALUES ('Nala', 3, 'F','Siamese', 'diva', 0);
+INSERT INTO cats (name, age, gender, breed, temperment, declawed) VALUES ('Grumpy Cat', 2, 'F','Ragdoll', 'friendly', 1);
+INSERT INTO cats (name, age, gender, breed, temperment, declawed) VALUES ('Lil Bub', 3, 'F','Dwarf', 'friendly', 1);
